6a41120b48739900b5a7ca6f3c9e93841b3e3d4f
John <ednanjhony@yahoo.com.br>
2024-03-08 06:52:48 -0300

    Doing order/order-table-row

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,20 +4,25 @@ import { AppLayout } from './pages/_layouts/app'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { AuthLayout } from './pages/_layouts/auth'
+import { SignUp } from './pages/auth/sign-up'
+import { Orders } from './pages/app/orders/orders'
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout />,
-        children: [{ path: '/', element: <Dashboard /> }],
+        children: [
+            { path: '/', element: <Dashboard /> },
+            { path: '/orders', element: <Orders /> },
+        ],
     },
 
     {
         path: '/',
         element: <AuthLayout />,
-        children: [{ path: '/sign-in', element: <SignIn /> }],
+        children: [
+            { path: '/sign-in', element: <SignIn /> },
+            { path: '/sign-up', element: <SignUp /> },
+        ],
     },
-    
-    // { path: '/', element: <Dashboard />},
-    // { path: '/sign-in', element: <SignIn />},
 ])
